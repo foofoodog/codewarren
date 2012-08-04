e7fa30c3cf90ea361bbe6648ea49477313451359
foofoodog <foofoodog@hotmail.com>
2012-08-04 11:39:29 -0400

    Testing 123

---
+++ Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SignalR.Hubs;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+
+
+public class Chat : Hub
+{
+    public void Send(string message)
+    {
+        Clients.addMessage(message);
+    }
+}
